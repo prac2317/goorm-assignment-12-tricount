3c214bf95c48ebd962f590806e18d3735072dd38
prac2317 <lowdy92@naver.com>
2024-09-07 21:04:42 +0900

    first commit

---
+++ src/test/java/com/example/tricount/TricountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tricount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
